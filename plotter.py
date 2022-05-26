4a66e15a8c02dd2dc5fd84f6932ef66e39eea96f
Kyrylo Sovailo <k.sovailo@gmail.com>
2022-05-26 15:40:30 +0200

    Plotter improved

--- plotter.py
+++ plotter.py
@@ -5,88 +5,131 @@ from franka_pole.msg import DebugSample
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-class Plotter:
+sample_frequency = 20
+
+class Signal:
+    # Creates signal
+    # label - Label on the plot
+    # source - Function that returns float value
+    def __init__(self, label, source):
+        self.label = label
+        self.source = source
+
+class Plot:
+    # Creates plot
+    # title - Title of the plot
+    # line - Vertical position on screen
+    # column - Horizontal position on screen
+    # signals - List of Signal objects
+    # lower - Lower limit of the plot
+    # upper - Upper limit of the plot
+    # time - Lower limit of the plot in time axis
+    def __init__(self, title, line, column, signals, lower, upper, time):
+        self.title = title
+        self.line = line
+        self.column = column
+        self.signals = signals
+        self.lower = lower
+        self.upper = upper
+        self.time = time
+
+        self.axes = []                                                      # matplotlib axes object, one for plot
+        self.time_buffer = np.linspace(-time, 0, time * sample_frequency)   # numpy buffer for time, one for plot
+        self.lines = []                                                     # matplotlib line object, one for plot and signal
+        self.buffers = []                                                   # numpy buffer, one for plot and signal
+        for signal in signals:
+            self.axes.append(None)
+            self.lines.append(None)
+            self.buffers.append(np.zeros(self.time * sample_frequency))
+
+# Samples data with given frequency    
+class Sampler:
     # Sets current sample data
-    def sample_callback(self, sample):
-        self._pole_angle = sample.pole_angle
-        self._pole_dangle = sample.pole_dangle
-        self._franka_effector_y = sample.franka_effector_y
-        self._franka_effector_dy = sample.franka_effector_dy
-        self._desired_acceleration = sample.desired_acceleration
+    def _sample_callback(self, sample):
+        self.sample = sample
 
-    # Samples data and plots it, called from Plot
-    def animate_callback(self, i):
-        # angle
-        self._pole_angle_buffer = np.roll(self._pole_angle_buffer, -1)
-        self._pole_angle_buffer[-1] = self._pole_angle
-        self._pole_angle_line.set_data(self._time, self._pole_angle_buffer)
-        # dangle
-        self._pole_dangle_buffer = np.roll(self._pole_dangle_buffer, -1)
-        self._pole_dangle_buffer[-1] = self._pole_dangle
-        self._pole_dangle_line.set_data(self._time, self._pole_dangle_buffer)
-        # effector_y
-        self._franka_effector_y_buffer = np.roll(self._franka_effector_y_buffer, -1)
-        self._franka_effector_y_buffer[-1] = self._franka_effector_y
-        self._franka_effector_y_line.set_data(self._time, self._franka_effector_y_buffer)
-        # efector_dy
-        self._franka_effector_dy_buffer = np.roll(self._franka_effector_dy_buffer, -1)
-        self._franka_effector_dy_buffer[-1] = self._franka_effector_dy
-        self._franka_effector_dy_line.set_data(self._time, self._franka_effector_dy_buffer)
-        # desired_acceleration
-        self._desired_acceleration_buffer = np.roll(self._desired_acceleration_buffer, -1)
-        self._desired_acceleration_buffer[-1] = self._desired_acceleration
-        self._desired_acceleration_line.set_data(self._time, self._desired_acceleration_buffer)
-        
-        return self._pole_angle_line, self._pole_dangle_line, self._franka_effector_y_line, self._franka_effector_dy_line, self._desired_acceleration_line
+    # Starts sampler
+    def __init__(self):
+        self.sample = DebugSample()
+        self.sample_subscriber = rospy.Subscriber("/franka_pole/samples", DebugSample, lambda sample: self._sample_callback(sample))
 
-    # Waits for termination
-    def spin(self):
+# Pltos data with given frequency
+class Plotter:
+    # Shifts buffers and returns lines
+    def _animate_callback(self):
+        lines = []
+        for plot in self.plots:
+            for i in range(len(plot.signals)):
+                plot.buffers[i] = np.roll(plot.buffers[i], -1)
+                plot.buffers[i][-1] = plot.signals[i].source()
+                plot.lines[i].set_data(plot.time_buffer, plot.buffers[i])
+                lines.append(plot.lines[i])
+        return lines
+
+    # Creates Plotter
+    def __init__(self, caption, plots):
+        self.caption = caption
+        self.plots = plots
+
+        # Check plots
+        max_line = 0
+        max_column = 0
+        for plot in plots:
+            if plot.line < 0: raise Exception("Line less than zero in " + plot.title)
+            if plot.column < 0: raise Exception("Line column than zero in " + plot.title)
+            if plot.line > max_line: max_line = plot.line
+            if plot.column > max_column: max_column = plot.column
+
+        plot_matrix = [[None for column in range(max_column+1)] for line in range(max_line+1)]
+        for plot in plots:
+            if plot_matrix[plot.line][plot.column] != None: raise Exception("Several plots on same position: " + plot_matrix[plot.line][plot.column].title + ", " + plot.title)
+            else: plot_matrix[plot.line][plot.column] = plot
+
+        for line in range(max_line+1):
+            for column in range(max_column+1):
+                if plot_matrix[line][column] == None: raise Exception("No plot on position: " + str(line) + ", " + str(column))
+
+        # Create figure and axes
+        self.figure, axes = plt.subplots(max_line+1, max_column+1, num = caption)
+        if len(plots) == 1: plots[0].axes = axes
+        else:
+            for i in range(len(plots)): plots[i].axes = axes[i]
+
+        # Set limits and titles
+        for plot in plots:
+            plot.axes.set_xlim([-plot.time, 0])
+            plot.axes.set_ylim([plot.lower, plot.upper])
+            plot.axes.set_title(plot.title)
+
+        # Create lines
+        for plot in plots:
+            for i in range(len(plot.signals)):
+                plot.lines[i], = plot.axes.plot([], [], label = plot.signals[i].label)
+
+        # Set legend positions
+        for plot in plots:
+            plot.axes.legend(loc = "upper left")
+
+        self.animation = animation.FuncAnimation(self.figure, lambda i: self._animate_callback(), interval=1000/sample_frequency, blit=True)
+
+    # Runs plotter
+    def start(self):        
         plt.show()
 
-    # Creates Plot
-    def __init__(self):
-        # Configuretion
-        self._sample_time = 10
-        self._sample_frequency = 20
-
-        # Creating lines
-        self._figure, self._axes = plt.subplots(1, 1, num="franka_pole")
-        self._axes = [ self._axes ] # If one subplot
-        self._axes[0].set_xlim([-self._sample_time, 0])
-        self._axes[0].set_ylim([-10,10])
-        #self._axes[0].set_title("title")
-        self._pole_angle_line, = self._axes[0].plot([], [], label="angle")
-        self._pole_dangle_line, = self._axes[0].plot([], [], label="dangle")
-        self._franka_effector_y_line, = self._axes[0].plot([], [], label="effector_y")
-        self._franka_effector_dy_line, = self._axes[0].plot([], [], label="effector_dy")
-        self._desired_acceleration_line, = self._axes[0].plot([], [], label="desired_acceleration")
-        self._axes[0].legend(loc="upper left")
-
-        # Creating buffers
-        self._time = np.linspace(-self._sample_time, 0, self._sample_time * self._sample_frequency)
-        self._pole_angle_buffer = np.zeros(self._sample_time * self._sample_frequency)
-        self._pole_dangle_buffer = np.zeros(self._sample_time * self._sample_frequency)
-        self._franka_effector_y_buffer = np.zeros(self._sample_time * self._sample_frequency)
-        self._franka_effector_dy_buffer = np.zeros(self._sample_time * self._sample_frequency)
-        self._desired_acceleration_buffer = np.zeros(self._sample_time * self._sample_frequency)
-
-        self._pole_angle = 0
-        self._pole_dangle = 0
-        self._franka_effector_y = 0
-        self._franka_effector_dy = 0
-        self._desired_acceleration = 0
-
-        # Starting subscriber
-        self._sample_subscriber = rospy.Subscriber("/franka_pole/samples", DebugSample, lambda sample: self.sample_callback(sample))
-
-        # Starting animation
-        self._animation = animation.FuncAnimation(self._figure, lambda i: self.animate_callback(i), interval=1000/self._sample_frequency, blit=True)
 
 if __name__ == '__main__':
     rospy.init_node('plotter')
-
-    # Creating plotter
-    plotter = Plotter()
-
-    # Starting plotter
-    plotter.spin()
+    
+    sampler = Sampler()
+    
+    pole_angle = Signal("Pole angle", lambda: sampler.sample.pole_angle)
+    pole_dangle = Signal("Pole rotaion velocity", lambda: sampler.sample.pole_dangle)
+    franka_effector_y = Signal("Effector Y", lambda: sampler.sample.franka_effector_y)
+    franka_effector_dy = Signal("Effector Y velocity", lambda: sampler.sample.franka_effector_dy)
+    desired_acceleration = Signal("Desired Acceleration", lambda: sampler.sample.desired_acceleration)
+    
+    plot = Plot("All", 0, 0, [ pole_angle, pole_dangle, franka_effector_y, franka_effector_dy, desired_acceleration ], -10, 10, 10)
+    
+    plotter = Plotter("Plotter", [ plot ])
+    
+    plotter.start()
