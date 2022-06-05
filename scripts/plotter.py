395e16d11d7315ccf1274cdd805fd8787fbd57f8
Kyrylo Sovailo <k.sovailo@gmail.com>
2022-06-05 15:14:24 +0200

    Acceleration filtered

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -5,51 +5,50 @@ from franka_pole.msg import Sample
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
-sample_frequency = 50
-
-# Listens all relevant topics
 class Subscriber:
-    # Sets current sample data
+    """Listens all relevant topics"""
+
     def _sample_callback(self, sample):
-        # Shift franka variables
-        if sample.franka_timestamp != self.previous_sample.franka_timestamp:
-            self.previous_sample.franka_timestamp = self.sample.franka_timestamp
-            self.previous_sample.franka_effector_y = self.sample.franka_effector_y
-            self.previous_sample.franka_effector_dy = self.sample.franka_effector_dy
+        """Sets current sample
+         - sample - received sample"""
 
-        # Getting sample
+        self.previous_filtered_effector_dy = self.filtered_effector_dy
+        self.filtered_effector_dy = 0.95 * self.filtered_effector_dy + 0.05 * sample.franka_effector_dy
+
+        self.previous_franka_timestamp = self.sample.franka_timestamp
         self.sample = sample
-
 
     # Starts subscriber
     def __init__(self):
+        """Initializes subscriber"""
         self.sample = Sample()
-        self.previous_sample = Sample()
-        self.previous_sample.franka_timestamp = -np.Inf
-        self.previous_sample.pole_timestamp = -np.Inf
-        self.previous_sample.control_timestamp = -np.Inf
+        self.filtered_effector_dy = 0.0
+        self.previous_franka_timestamp = -np.Inf
+        self.previous_filtered_effector_dy = 0.0
         self.sample_subscriber = rospy.Subscriber("/franka_pole/sample", Sample, lambda sample: self._sample_callback(sample))
 
 class Signal:
-    # Creates signal
-    # label - Label on the plot
-    # source - Function that returns float value
-    def __init__(self, label, source, filter_factor = 0.0):
+    """Represents one real value real-time signal"""
+
+    def __init__(self, label, source):
+        """Creates signal
+         - label - label to be placed on the plot
+         - source - function that returns float value"""
         self.label = label
         self.source = source
-        self.filter_factor = filter_factor
-        self.value = 0.0
 
 class Plot:
-    # Creates plot
-    # title - Title of the plot
-    # line - Vertical position on screen
-    # column - Horizontal position on screen
-    # signals - List of Signal objects
-    # lower - Lower limit of the plot
-    # upper - Upper limit of the plot
-    # time - Lower limit of the plot in time axis
+    """Represents real-time plot with time as X axis and signals as Y axis"""
+
     def __init__(self, title, line, column, signals, lower, upper, time):
+        """Creates plot
+         - title - title of the plot
+         - line - vertical position on the window
+         - column - horizontal position on the window
+         - signals - list of signal objects
+         - lower - lower bound of Y axis
+         - upper - upper bound of Y axis
+         - time - -time is lower bound of X axis, upper boind is zero"""
         self.title = title
         self.line = line
         self.column = column
@@ -58,23 +57,50 @@ class Plot:
         self.upper = upper
         self.time = time
 
-        self.axes = None                                                    # matplotlib axes object, one for plot
-        self.time_buffer = np.linspace(-time, 0, time * sample_frequency)   # numpy buffer for time, one for plot
-        self.lines = []                                                     # matplotlib line object, one for plot and signal
-        self.buffers = []                                                   # numpy buffer, one for plot and signal
-        for signal in signals:
-            self.lines.append(None)
-            self.buffers.append(np.zeros(self.time * sample_frequency))
+        self._frequency = None
+        self._axes = None           # matplotlib axes object, one for plot
+        self._time_buffer = None    # numpy buffer for time, one for plot
+        self._lines = None          # matplotlib line object, one for plot and signal
+        self._buffers = None        # numpy buffer, one for plot and signal
+
+    def _init(self, frequency):
+        """Fully initializes plot, internal use only"""
+        # Create buffers
+        self._frequency = frequency
+        self._time_buffer = np.linspace(-self.time, 0, self.time * frequency)
+        self._lines = [ None for signal in self.signals ]
+        self._buffers = [ np.zeros(self.time * frequency) for signal in self.signals ]
+
+        # Set limits
+        self._axes.set_xlim([-self.time, 0])
+        self._axes.set_ylim([self.lower, self.upper])
+        self._axes.set_title(self.title)
+
+        # Create lines
+        for i in range(len(self.signals)): self._lines[i], = self._axes.plot([], [], label = self.signals[i].label)
+
+        # Set legend position
+        self._axes.legend(loc = "upper left")
+
+    def _animate(self):
+        """Returns lines, internal use only"""
+        for i in range(len(self.signals)):
+            self._buffers[i] = np.roll(self._buffers[i], -1)
+            self._buffers[i][-1] = self.signals[i].source()
+            self._lines[i].set_data(self._time_buffer, self._buffers[i])
+        return self._lines
 
 class BarChart:
-    # Creates bar chart
-    # title - Title of the bar chart
-    # line - Vertical position on screen
-    # column - Horizontal position on screen
-    # signals - List of Signal objects
-    # lower - Lower limit of the plot
-    # upper - Upper limit of the plot
+    """Represents real-time bar charts with multiple bars and signals as their heights"""
+
     def __init__(self, title, line, column, signals, lower, upper):
+        """Represents bar chart
+         - title - title of the bar chart
+         - line - vertical position on the window
+         - column - horizontal position on the window
+         - signals - list of signal objects
+         - lower - lower bound of Y axis
+         - upper - upper bound of Y axis"""
         self.title = title
         self.line = line
         self.column = column
@@ -82,31 +108,40 @@ class BarChart:
         self.lower = lower
         self.upper = upper
 
-        self.axes = None    # matplotlib axes object, one for plot
-        self.bars = None    # matplotlib bar container object, one for plot
+        self._frequency = None
+        self._axes = None       # matplotlib axes object, one for plot
+        self._bars = None       # matplotlib bar container object, one for plot
 
-# Pltos data with given frequency
+    def _init(self, frequency):
+        """Fully initializes bar chart, internal use only"""
+        self._frequency = frequency
+
+        # Set limits
+        self._axes.set_ylim([self.lower, self.upper])
+        self._axes.set_title(self.title)
+
+        # Create bars
+        self._bars = self._axes.bar([ signal.label for signal in self.signals ], np.zeros(len(self.signals)))
+
+    def _animate(self):
+        """Returns bars, internal use only"""
+        for i in range(len(self.signals)): self._bars[i].set_height(self.signals[i].source())
+        return self._bars
+
 class Plotter:
-    # Shifts buffers and returns lines
+    """Plotter, plots data with given frequency"""
+    
     def _animate_callback(self):
+        """Shifts buffers and returns lines, internal use only"""
         objects = []
-        for plot in self.plots:
-            if type(plot) is BarChart:
-                for i in range(len(plot.signals)):
-                    plot.signals[i].value = plot.signals[i].filter_factor * plot.signals[i].value + (1.0 - plot.signals[i].filter_factor) * plot.signals[i].source()
-                    plot.bars[i].set_height(plot.signals[i].value)
-                    objects.append(plot.bars[i])
-            else:
-                for i in range(len(plot.signals)):
-                    plot.buffers[i] = np.roll(plot.buffers[i], -1)
-                    plot.signals[i].value = plot.signals[i].filter_factor * plot.signals[i].value + (1.0 - plot.signals[i].filter_factor) * plot.signals[i].source()
-                    plot.buffers[i][-1] = plot.signals[i].value
-                    plot.lines[i].set_data(plot.time_buffer, plot.buffers[i])
-                    objects.append(plot.lines[i])
+        for plot in self.plots: objects.extend(plot._animate())
         return objects
 
-    # Creates Plotter
-    def __init__(self, caption, plots):
+    def __init__(self, caption, plots, frequency):
+        """Creates plotter
+         - caption - caption of the window
+         - plots - list of plots
+         - frequency - plotting frequency"""
         self.caption = caption
         self.plots = plots
 
@@ -130,37 +165,20 @@ class Plotter:
 
         # Create figure and axes
         self.figure, axes = plt.subplots(max_line+1, max_column+1, num = caption)
-        if len(plots) == 1: plots[0].axes = axes
+        if len(plots) == 1:
+            plots[0]._axes = axes
         else:
-            for i in range(len(plots)): plots[i].axes = axes[i]
+            for i in range(len(plots)): plots[i]._axes = axes[i]
 
-        # Set limits and titles
-        for plot in plots:
-            if not type(plot) is BarChart: plot.axes.set_xlim([-plot.time, 0])
-            plot.axes.set_ylim([plot.lower, plot.upper])
-            plot.axes.set_title(plot.title)
+        # Fully initialize plots
+        for plot in plots: plot._init(frequency)
 
-        # Create lines/bars
-        for plot in plots:
-            if type(plot) is BarChart:
-                labels = []
-                for i in range(len(plot.signals)):
-                    labels.append(plot.signals[i].label)
-                plot.bars = plot.axes.bar(labels, np.ones(len(plot.signals)))
-            else:
-                for i in range(len(plot.signals)):
-                    plot.lines[i], = plot.axes.plot([], [], label = plot.signals[i].label)
+        # Create animation
+        self.animation = animation.FuncAnimation(self.figure, lambda i: self._animate_callback(), interval=1000/frequency, blit=True)
 
-        # Set legend positions
-        for plot in plots:
-            if not type(plot) is BarChart: plot.axes.legend(loc = "upper left")
-
-        self.animation = animation.FuncAnimation(self.figure, lambda i: self._animate_callback(), interval=1000/sample_frequency, blit=True)
-
-    # Runs plotter
-    def start(self):        
+    def start(self):
+        """Runs plotter"""
         plt.show()
-
 
 if __name__ == '__main__':
     rospy.init_node('plotter')
@@ -171,13 +189,13 @@ if __name__ == '__main__':
     pole_dangle = Signal("Pole rotaion velocity", lambda: subscriber.sample.pole_dangle)
     franka_effector_y = Signal("Effector Y", lambda: subscriber.sample.franka_effector_y)
     franka_effector_dy = Signal("Effector Y velocity", lambda: subscriber.sample.franka_effector_dy)
-    franka_effector_ddy = Signal("Effector Y acceleration", lambda: (subscriber.sample.franka_effector_dy - subscriber.previous_sample.franka_effector_dy) / (subscriber.sample.franka_timestamp - subscriber.previous_sample.franka_timestamp), 0.8)
+    franka_effector_ddy = Signal("Effector Y acceleration", lambda: (subscriber.filtered_effector_dy - subscriber.previous_filtered_effector_dy) / (subscriber.sample.franka_timestamp - subscriber.previous_franka_timestamp))
     control_effector_ddy = Signal("Desired Y acceleration", lambda: subscriber.sample.control_effector_ddy)
     
     position_plot = Plot("Position", 0, 0, [ pole_angle, franka_effector_y ], -1, 1, 3)
     velocity_plot = Plot("Velocity", 0, 1, [ pole_dangle, franka_effector_dy ], -5, 5, 3)
     acceleration_plot = Plot("Acceleration", 0, 2, [ franka_effector_ddy, control_effector_ddy ], -50, 50, 3)
     
-    plotter = Plotter("Plotter", [ position_plot, velocity_plot, acceleration_plot ])
+    plotter = Plotter("Plotter", [ position_plot, velocity_plot, acceleration_plot ], 50)
     
     plotter.start()
