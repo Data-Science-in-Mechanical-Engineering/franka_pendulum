c430340bf87af068ab7b263d701439fe0548db9c
Kyrylo Sovailo <k.sovailo@gmail.com>
2022-08-12 11:23:51 +0200

    Forgotten run_parallel.sh modification

--- scripts/run_parallel.sh
+++ scripts/run_parallel.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 PIDFILE=$(rospack find franka_pole)/temp/run.pid
-if [ "$1" = "kill" ]; then
+if [ "$1" = "start" -o "$1" = "stop" ]; then
     if [ -f "${PIDFILE}" ]; then
         while read -r pid; do kill $pid; done < "${PIDFILE}"
         rm "${PIDFILE}"
     fi
 else
+    echo Invalid usage
+fi
+
+if [ "$1" = "start" ]; then
     COMMAND='roslaunch franka_pole franka_pole.launch model:=2Db'
 
     # Simulation
