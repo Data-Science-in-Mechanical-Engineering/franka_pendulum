cd2246e29a4659481fea2b6b30d79627c0305731
Kyrylo Sovailo <k.sovailo@gmail.com>
2022-08-22 13:45:48 +0200

    resetter runable from command line

--- scripts/resetter.py
+++ scripts/resetter.py
@@ -6,22 +6,35 @@ import sys
 # Callback for timer
 def callback(event):
     command = CommandReset()
-    command.software = True
+    command.software = not hardware
     publisher.publish(command)
 
 # Main
 if __name__ == "__main__":
     # Read arguments
     namespace = "franka_pole"
+    period = None
+    hardware = False
     next_namespace = False
+    next_period = False
     for i in sys.argv:
         if next_namespace: namespace = i
+        elif next_period: period = float(i)
         next_namespace = (i == "-N")
+        next_period = (i == "-T")
+        if i == "-H": hardware = True
 
     # Initialize
     rospy.init_node(namespace + "_resetter")
     publisher = rospy.Publisher("/" + namespace + "/command_reset", CommandReset, queue_size=10)
     
     # Run
-    timer = rospy.Timer(rospy.Duration(10), callback)
-    rospy.spin()
+    if period is None:
+        rospy.sleep(1.0)
+        command = CommandReset()
+        command.software = not hardware
+        publisher.publish(command)
+    
+    else:
+        timer = rospy.Timer(rospy.Duration(period), callback)
+        rospy.spin()
