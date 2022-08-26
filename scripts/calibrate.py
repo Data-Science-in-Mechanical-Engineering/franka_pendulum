b89f2ce760063ef98077ddd57a1e13f77ebab834
Kyrylo Sovailo <k.sovailo@gmail.com>
2022-08-26 19:44:41 +0200

    Bugfixes

--- scripts/calibrate.py
+++ scripts/calibrate.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from turtle import position
 from franka_pole.msg import Sample, CommandParameters
 import numpy as np
 import rospy
@@ -7,14 +6,19 @@ import sys
 
 # Callback for receiving samples
 def callback(sample):
-    global position, counter, duration
+    global counter, duration, angle, position
     counter += 1
+    angle += sample.pole_angle
     position += sample.franka_effector_position
     if counter >= duration:
         counter = 0
+        angle /= duration
         position /= duration
-        parameters.pole_angle_mean[0] += (position[0] - parameters.target_effector_position[0]) * parameters.pole_control[2] / parameters.pole_control[0]
-        parameters.pole_angle_mean[1] += (position[1] - parameters.target_effector_position[1]) * parameters.pole_control[6] / parameters.pole_control[4]
+        parameters.pole_angle_mean = np.array([
+            parameters.pole_angle_mean[0] - angle[0],
+            parameters.pole_angle_mean[1] - angle[1]
+        ])
+        print(parameters.pole_angle_mean)
         parameters_publisher.publish(parameters)
         position = np.zeros(3)
 
@@ -29,12 +33,13 @@ if __name__ == '__main__':
 
     # Initialize
     rospy.init_node(namespace + "_calibrate")
+    counter = 0
+    duration = 10*100
+    angle = np.zeros(2)
+    position = np.zeros(3)
     sample_subscriber = rospy.Subscriber("/" + namespace + "/sample", Sample, callback)
     parameters_publisher = rospy.Publisher("/" + namespace + "/command_parameters", CommandParameters, queue_size=10)
     parameters = rospy.wait_for_message("/" + namespace + "/command_parameters", CommandParameters)
-    position = np.zeros(3)
-    counter = 0
-    duration = 10*1000
 
     # Run
     rospy.spin()
